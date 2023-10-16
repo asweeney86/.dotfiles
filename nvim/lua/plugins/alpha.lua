864d0208dda6174da04c7653b2e07f4c424b5df4
Andrew Sweeney <asweeney86@gmail.com>
2023-10-16 09:23:29 -0400

    Update format

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,14 +1,16 @@
 return {
-  "glepnir/dashboard-nvim",
+  "nvimdev/dashboard-nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function(_, opts)
     opts.config.header = {
       "",
-      "  _____         __           ",
-      " / ___/__ _____/ /  ___  ___ ",
-      "/ /__/ _ `/ __/ _ / _ / _  ",
-      "___/_,_/_/ /_.__/___/_//_/ ",
-      "                              ",
+      "                 _                 ",
+      "                | |                ",
+      "   ___ __ _ _ __| |__   ___  _ __  ",
+      " / __/ _` | '__| '_ \\ / _ \\| '_ \\",
+      " | (_| (_| | |  | |_) | (_) | | | |",
+      " \\___\\__,_|_|  |_.__/ \\___/|_| |_|",
+      "                                   ",
       "",
     }
   end,
