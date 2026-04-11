4898c7369b841d8e48cb2ddc484992ad76a5e59a
Andrew Sweeney <asweeney86@gmail.com>
2026-04-11 08:01:20 -0400

    fix(nvim): remove obsolete formatOnSave option

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -71,8 +71,6 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      formatOnSave = true,
-
       -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
       -- Be aware that you also will need to properly configure your LSP server to
       -- provide the inlay hints.
