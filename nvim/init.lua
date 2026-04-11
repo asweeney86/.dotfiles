d075c932b864a3c597f5310854bc95ab136c427d
Andrew Sweeney <asweeney86@gmail.com>
2026-04-11 07:55:57 -0400

    fix(nvim): use portable python host path

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.python3_host_prog = "/Users/andrewsweeney/.dotfiles/nvim/python/.venv/bin/python"
-vim.g.python_host_prog = "/Users/andrewsweeney/.dotfiles/nvim/python/.venv/bin/python"
+
+local python_host = vim.fn.expand("~/.dotfiles/nvim/python/.venv/bin/python")
+if vim.fn.executable(python_host) == 1 then
+  vim.g.python3_host_prog = python_host
+end
 
 require("config.lazy")
